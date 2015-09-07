391d6397262a445963c09e011eea40d4a23eec53
Bob Fisch <bob@fisch.lu>
2015-09-07 14:24:44 +0200

    BobDue fixed!

--- app/src/main/java/lu/fisch/canze/devices/BobDue.java
+++ app/src/main/java/lu/fisch/canze/devices/BobDue.java
@@ -15,6 +15,9 @@ import lu.fisch.canze.actors.Utils;
 public class BobDue extends Device {
 
     // *** needed by the "reader" part
+    private String buffer = "";
+    private final String separator = "\r\n";
+
 
     // define the timeout we may wait to get an answer
     private static final int TIMEOUT = 500;
@@ -89,8 +92,91 @@ public class BobDue extends Device {
 
     @Override
     protected ArrayList<Message> processData(int[] input) {
+        ArrayList<Message> result = new ArrayList<>();
+
+        // add to buffer as characters
+        for (int i = 0; i < input.length; i++) {
+            buffer += (char) input[i];
+        }
+
+        // split by <new line>
+        String[] messages = buffer.split(separator);
+        // let assume the last message is fine
+        int last = messages.length;
+        // but if it is not, do not consider it
+        if (!buffer.endsWith(separator)) last--;
+
+        // process each message
+        for (int i = 0; i < last; i++) {
+            // decode into a frame
+            Message message = decodeFrame(messages[i].trim());
+            // store if valid
+            if (message != null)
+                result.add(message);
+        }
+        // adapt the buffer
+        if (!buffer.endsWith(separator))
+            // retain the last uncompleted message
+            buffer = messages[messages.length - 1];
+        else
+            // empty the entire buffer
+            buffer = "";
+        // we are done
+
+        return result;
+    }
+
+    private Message decodeFrame(String text) {
+        // split up the fields
+        String[] pieces = text.split(",");
+        if(pieces.length==2) {
+            try {
+                // get the id
+                int id = Integer.parseInt(pieces[0], 16);
+                // get the data
+                int[] data = Utils.toIntArray(pieces[1].trim());
+                // create and return new frame
+                return new Message(id, data);
+            }
+            catch(Exception e)
+            {
+                //MainActivity.debug("BAD: "+text);
+                return null;
+            }
+        }
+        else if(pieces.length==3) {
+            try {
+                // get the id
+                int id = Integer.parseInt(pieces[0], 16);
+                // get the data
+                int[] data = Utils.toIntArray(pieces[1].trim());
+                // get the reply-ID
+                Message f = new Message(id,data);
+                //MainActivity.debug("THIRD: "+pieces[2].trim());
+                f.setResponseId(pieces[2].trim());
+                return f;
+                /*
+                // get checksum
+                int chk = Integer.parseInt(pieces[2].trim(), 16);
+                int check = 0;
+                for(int i=0; i<data.length; i++)
+                    check ^= data[i];
+                // validate the checksum
+                if(chk==check)
+                    // create and return new frame
+                    return new Frame(id, data);
+                */
+            }
+            catch(Exception e)
+            {
+                //MainActivity.debug("BAD: "+text);
+                return null;
+            }
+        }
+        //MainActivity.debug("BAD: "+text);
         return null;
     }
+
 
     // query the device for the next filter
     private void queryNextFilter()
@@ -116,7 +202,6 @@ public class BobDue extends Device {
                         // request the response from the device
                         //MainActivity.debug("Requesting: " + filter);
                         String hexData = sendAndWaitForAnswer("g" + filter, 0);
-                        MainActivity.debug(hexData);
                         // process data
                         process(Utils.toIntArray(hexData.getBytes()));
                     }
