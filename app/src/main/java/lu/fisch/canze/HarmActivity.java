514eb9c0559fb42a6fceb0c55de81afc4debd010
yoh-there <meijer.jwm@gmail.com>
2015-10-10 20:05:15 +0200

    Added Harm's playground

---
+++ app/src/main/java/lu/fisch/canze/HarmActivity.java
@@ -0,0 +1,85 @@
+package lu.fisch.canze;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import lu.fisch.canze.actors.Field;
+import lu.fisch.canze.actors.Fields;
+import lu.fisch.canze.interfaces.FieldListener;
+
+/**
+ * Created by jeroen on 10-10-15.
+ */
+public class HarmActivity  extends CanzeActivity implements FieldListener {
+
+
+    private ArrayList<Field> subscribedFields;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harm);
+
+        subscribedFields = new ArrayList<>();
+
+
+        addListener("some ID from FIELDS inti code");
+    }
+
+    private void addListener(String sid) {
+        Field field;
+        field = MainActivity.fields.getBySID(sid);
+        if (field != null) {
+            field.addListener(this);
+            MainActivity.device.addField(field);
+            subscribedFields.add(field);
+        } else {
+            MainActivity.toast("sid " + sid + " does not exist in class Fields");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // free up the listeners again
+        for (Field field : subscribedFields) {
+            field.removeListener(this);
+        }
+        subscribedFields.clear();
+    }
+
+
+    public void onFieldUpdateEvent(final Field field) {
+
+        // this is called when the car sends a message frame with an ID that has been registered as listener
+        // check other activities for examples
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_text, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
