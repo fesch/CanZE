382a53105144f1c7903f92deb6b8fbadcef8db80
Robert Fisch <robertfisch@MACFISCH.home.dyndns.fisch.lu>
2015-11-06 16:52:03 +0100

    naming convention ;-)

--- app/src/main/java/lu/fisch/canze/actors/Frame.java
+++ app/src/main/java/lu/fisch/canze/actors/Frame.java
@@ -5,18 +5,18 @@ package lu.fisch.canze.actors;
  */
 public class Frame {
 
-    private int Id;
+    private int id;
     private int interval; // in ms
     private Ecu sendingEcu;
 
-    public Frame (int Id, int interval, Ecu sendingEcu) {
-        this.Id = Id;
+    public Frame (int id, int interval, Ecu sendingEcu) {
+        this.id = id;
         this.interval = interval;
         this.sendingEcu = sendingEcu;
     }
 
     public int getId() {
-        return Id;
+        return id;
     }
 
     public int getInterval() {
