9c8825f9bf3bcb8bdf6e2d5c061f97456c71fd6b
yoh-there <meijer.jwm@gmail.com>
2016-02-28 01:10:40 +0100

    Rounded km to miles conversion

--- app/src/main/java/lu/fisch/canze/activities/PredictionActivity.java
+++ app/src/main/java/lu/fisch/canze/activities/PredictionActivity.java
@@ -14,6 +14,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import lu.fisch.canze.R;
+import lu.fisch.canze.actors.Battery;
 import lu.fisch.canze.widgets.Plotter;
 import lu.fisch.canze.widgets.WidgetView;
 
@@ -23,10 +24,19 @@ public class PredictionActivity extends AppCompatActivity {
     private WidgetView widgetView;
     private Plotter plotter;
 
+    private Battery battery;
+    private int graphToShow = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prediction);
+
+        // initialize the battery, now with fixed values
+        // need to change this to get parameters from the car of course
+
+        battery = new Battery();
+        battery.setChargerPower (22.0);
 
         // get the widget view
         widgetView = (WidgetView) findViewById(R.id.predictionPlotter);
@@ -42,11 +52,35 @@ public class PredictionActivity extends AppCompatActivity {
                 if (plotter != null) {
                     // generate some values
                     ArrayList<Double> values = new ArrayList<>();
-                    Random random = new Random();
-                    values.add(random.nextDouble()*100);
-                    for (int i = 0; i < 100; i++) {
-                        values.add(Math.max(Math.min((1+(random.nextDouble()/4-1/8.)) * values.get(values.size()-1),100),0));
+
+                    battery.setTemperature (10.0);
+                    battery.setStateOfCharge(5.0);
+                    for (int t = 1; t <=100; t++) { // 100 ticks
+                        battery.iterateCharging((int)(1000 / battery.getChargerPower())); // @ 43kW a tick is roughly 23 seconds, total time 2300 sec = 38 min
+                        switch (graphToShow) {
+                            case 0:
+                                values.add(battery.getDcPower() * 2.5);
+                                break;
+                            case 1:
+                                values.add(battery.getMaxDcPower() * 2.5);
+                                break;
+                            case 2:
+                                values.add(battery.getStateOfCharge() * 4.0);
+                                break;
+                            case 3:
+                                values.add(battery.getTemperature() * 4.0 - 30);
+                                break;
+                        }
+                        //draw (battery, t); // imaginary method that plots SOC, range, time
                     }
+
+                    if (++graphToShow > 3) graphToShow = 0;
+//
+//                    Random random = new Random();
+//                    values.add(random.nextDouble()*100);
+//                    for (int i = 0; i < 100; i++) {
+//                        values.add(Math.max(Math.min((1+(random.nextDouble()/4-1/8.)) * values.get(values.size()-1),100),0));
+//                    }
 
                     // set the values
                     plotter.setValues(values);
