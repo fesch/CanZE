38e16b32fafe9c66fe292532ed7068c734ee0218
yoh-there <meijer.jwm@gmail.com>
2016-12-31 14:23:31 +0100

    Better String formatting in charging and chargingTech

--- app/src/main/java/lu/fisch/canze/activities/ChargingTechActivity.java
+++ app/src/main/java/lu/fisch/canze/activities/ChargingTechActivity.java
@@ -24,6 +24,8 @@ package lu.fisch.canze.activities;
 import android.os.Bundle;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Locale;
+
 import lu.fisch.canze.R;
 import lu.fisch.canze.actors.Field;
 import lu.fisch.canze.interfaces.FieldListener;
@@ -34,8 +36,8 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
 
     public static final String SID_MaxCharge                        = "7bb.6101.336";
     public static final String SID_UserSoC                          = "42e.0";          // user SOC, not raw
-//   public static final String SID_RealSoC                          = "654.25";         // real SOC
-    public static final String SID_AvChargingPower                  = "427.40";
+//  public static final String SID_RealSoC                          = "654.25";         // real SOC
+    public static final String SID_AvailableChargingPower           = "427.40";
     public static final String SID_ACPilot                          = "42e.38";
     public static final String SID_AvEnergy                         = "427.49";
 
@@ -55,6 +57,12 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
     public static final String SID_HvKilometers                     = "7bb.6161.96";
     public static final String SID_Preamble_CompartmentTemperatures = "7bb.6104."; // (LBC)
     public static final String SID_Preamble_BalancingBytes          = "7bb.6107.";
+
+    public static final String DefaultFormatTemperature             = "%3.0f°";
+    public static final String DefaultFormatBalancingFirst          = "%02X";
+    public static final String DefaultFormatBalancing               = "-%02X";
+
+
 
 
     public static final String cha_Status [] = {"No charge", "Waiting (planned)", "Ended", "In progress", "Failure", "Waiting", "Flap open", "Unavailable"};
@@ -97,7 +105,7 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
         addListener(SID_UserSoC);
         addListener(SID_RealSoC);
         if (MainActivity.isZOE()) {
-            addListener(SID_AvChargingPower);
+            addListener(SID_AvailableChargingPower);
         //} else {
         //    addListener(SID_CapacityFluKan);
         }
@@ -171,7 +179,7 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
                             tv = (TextView) findViewById(R.id.textPhases);
                             tv.setText("1");
                             tv = (TextView) findViewById(R.id.textAvChPwr);
-                            tv.setText("" + (avChPwr));
+                            tv.setText(String.format(Locale.getDefault(), "%.1f", avChPwr));
                         }
                         // continue
                         tv = (TextView) findViewById(R.id.text_max_pilot);
@@ -184,8 +192,10 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
                         tv = (TextView) findViewById(R.id.textTTF);
                         if (field.getValue() >= 1023) {
                             tv.setText("--:--");
-                            tv = null;
+                        } else {
+                            tv.setText(String.format(Locale.getDefault(), "%.0f", field.getValue()));
                         }
+                        tv = null;
                         break;
                     case SID_UserSoC:
                         soc = field.getValue() / 100.0;
@@ -197,13 +207,15 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
                         break;
                     case SID_SOH:
                         tv = (TextView) findViewById(R.id.textSOH);
+                        tv.setText(String.format(Locale.getDefault(), "%.0f", field.getValue()));
+                        tv = null;
                         break;
                     case SID_RangeEstimate:
                         tv = (TextView) findViewById(R.id.textKMA);
                         if (field.getValue() >= 1023) {
                             tv.setText("---");
                         } else {
-                            tv.setText("" + field.getValue());
+                            tv.setText(String.format(Locale.getDefault(), "%.0f", field.getValue()));
                         }
                         tv = null;
                         break;
@@ -217,11 +229,11 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
                         // calculate DC power
                         double dcPwr = (double)Math.round(dcVolt * field.getValue() / 100.0) / 10.0;
                         tv = (TextView) findViewById(R.id.textDcPwr);
-                        tv.setText("" + (dcPwr));
+                        tv.setText(String.format(Locale.getDefault(), "%.1f", dcPwr));
                         // continue
                         tv = (TextView) findViewById(R.id.textAmps);
                         break;
-                    case SID_AvChargingPower: // won't be called for FluKan
+                    case SID_AvailableChargingPower: // won't be called for FluKan
                         double avChPwr = field.getValue();
                         tv = (TextView) findViewById(R.id.textPhases);
                         if (pilot == 0) {
@@ -236,28 +248,23 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
                     case SID_AvEnergy:
                         if (soc > 0) {
                             tv = (TextView) findViewById(R.id.textETF);
-                            tv.setText("" + (Math.round((field.getValue() * (1-soc) / soc) * 10.0) / 10.0));
+                            tv.setText(String.format(Locale.getDefault(), "%.1f", field.getValue() * (1-soc) / soc));
+                            //tv.setText("" + (Math.round((field.getValue() * (1-soc) / soc) * 10.0) / 10.0));
                         }
                         tv = (TextView) findViewById(R.id.textAvEner);
                         break;
                     case SID_12V:
                         tv = (TextView) findViewById(R.id.text12V);
-                        tv.setText("" + field.getValue());
-                        tv = null;
                         break;
                     case SID_12A:
                         tv = (TextView) findViewById(R.id.text12A);
-                        tv.setText("" + field.getValue());
-                        tv = null;
                         break;
                     case SID_DcLoad:
                         tv = (TextView) findViewById(R.id.textDcLoad);
-                        tv.setText("" + field.getValue());
-                        tv = null;
                         break;
                     case SID_HvKilometers:
                         tv = (TextView) findViewById(R.id.textHKM);
-                        tv.setText("" + field.getValue());
+                        tv.setText(String.format(Locale.getDefault(), "%.0f", field.getValue()));
                         tv = null;
                         break;
                     //case SID_CapacityFluKan:
@@ -279,128 +286,128 @@ public class ChargingTechActivity extends CanzeActivity implements FieldListener
                         break;
                     case SID_Preamble_CompartmentTemperatures + "32":
                         tv = (TextView) findViewById(R.id.text_comp_1_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "56":
                         tv = (TextView) findViewById(R.id.text_comp_2_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "80":
                         tv = (TextView) findViewById(R.id.text_comp_3_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "104":
                         tv = (TextView) findViewById(R.id.text_comp_4_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "128":
                         tv = (TextView) findViewById(R.id.text_comp_5_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "152":
                         tv = (TextView) findViewById(R.id.text_comp_6_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "176":
                         tv = (TextView) findViewById(R.id.text_comp_7_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "200":
                         tv = (TextView) findViewById(R.id.text_comp_8_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "224":
                         tv = (TextView) findViewById(R.id.text_comp_9_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "248":
                         tv = (TextView) findViewById(R.id.text_comp_10_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "272":
                         tv = (TextView) findViewById(R.id.text_comp_11_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_CompartmentTemperatures + "296":
                         tv = (TextView) findViewById(R.id.text_comp_12_temp);
-                        tv.setText(String.format("%.0f", field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatTemperature, field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "16":
                         tv = (TextView) findViewById(R.id.text_bala_1_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancingFirst, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "24":
                         tv = (TextView) findViewById(R.id.text_bala_2_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "32":
                         tv = (TextView) findViewById(R.id.text_bala_3_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "40":
                         tv = (TextView) findViewById(R.id.text_bala_4_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "48":
                         tv = (TextView) findViewById(R.id.text_bala_5_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "56":
                         tv = (TextView) findViewById(R.id.text_bala_6_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "64":
                         tv = (TextView) findViewById(R.id.text_bala_7_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancingFirst, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "72":
                         tv = (TextView) findViewById(R.id.text_bala_8_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "80":
                         tv = (TextView) findViewById(R.id.text_bala_9_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "88":
                         tv = (TextView) findViewById(R.id.text_bala_10_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "96":
                         tv = (TextView) findViewById(R.id.text_bala_11_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                     case SID_Preamble_BalancingBytes + "104":
                         tv = (TextView) findViewById(R.id.text_bala_12_temp);
-                        tv.setText(String.format("%02X", (int)field.getValue()));
+                        tv.setText(String.format(Locale.getDefault(), DefaultFormatBalancing, (int)field.getValue()));
                         tv = null;
                         break;
                 }
                 // set regular new content, all exeptions handled above
                 if (tv != null) {
-                    tv.setText(String.format("%.1f", field.getValue()));
+                    tv.setText(String.format(Locale.getDefault(), "%.1f", field.getValue()));
                 }
 
                 tv = (TextView) findViewById(R.id.textDebug);
