af0bf82b44611345e72ed63ecd0231dd62778bbc
Notice B.V <jmeijer@notice.nu>
2019-02-17 16:10:56 +0100

    Fixed error in BCB (uppercase hex :-( ) and inject Testerpresent-Awake

--- app/src/main/java/lu/fisch/canze/activities/BcbActivity.java
+++ app/src/main/java/lu/fisch/canze/activities/BcbActivity.java
@@ -28,6 +28,7 @@ import java.util.Locale;
 
 import lu.fisch.canze.R;
 import lu.fisch.canze.actors.Field;
+import lu.fisch.canze.actors.Frames;
 import lu.fisch.canze.interfaces.DebugListener;
 import lu.fisch.canze.interfaces.FieldListener;
 import lu.fisch.canze.activities.MainActivity;
@@ -36,14 +37,17 @@ import lu.fisch.canze.activities.MainActivity;
 // For the simple activity, the easiest way is to implement it in the actitviy itself.
 public class BcbActivity extends CanzeActivity implements FieldListener, DebugListener {
 
+    public static final String SID_TesterInit                   = "793.50c0.0";
+    public static final String SID_TesterAwake                  = "793.7e01.0";
+
     public static final String SID_MainsCurrentType             = "793.625017.29";
     public static final String SID_Phase1currentRMS             = "793.622001.24";
-    public static final String SID_Phase2CurrentRMS             = "793.62503A.24"; // Raw <= this seems to be instant DC coupled value
-    public static final String SID_Phase3CurrentRMS             = "793.62503B.24";
-    public static final String SID_PhaseVoltage1                = "793.62502C.24"; // Raw
-    public static final String SID_PhaseVoltage2                = "793.62502D.24";
-    public static final String SID_PhaseVoltage3                = "793.62502E.24";
-    public static final String SID_InterPhaseVoltage12          = "793.62503F.24"; // Measured
+    public static final String SID_Phase2CurrentRMS             = "793.62503a.24"; // Raw <= this seems to be instant DC coupled value
+    public static final String SID_Phase3CurrentRMS             = "793.62503b.24";
+    public static final String SID_PhaseVoltage1                = "793.62502c.24"; // Raw
+    public static final String SID_PhaseVoltage2                = "793.62502d.24";
+    public static final String SID_PhaseVoltage3                = "793.62502e.24";
+    public static final String SID_InterPhaseVoltage12          = "793.62503f.24"; // Measured
     public static final String SID_InterPhaseVoltage23          = "793.625041.24";
     public static final String SID_InterPhaseVoltage31          = "793.625042.24";
     public static final String SID_MainsActivePower             = "793.62504a.24";
@@ -51,9 +55,9 @@ public class BcbActivity extends CanzeActivity implements FieldListener, DebugLi
     public static final String SID_SupervisorState              = "793.625063.24";
     public static final String SID_CompletionStatus             = "793.625064.24";
 
-    final String mains_Current_Type [] = MainActivity.getInstance().getStringList(R.array.list_MainsCurrentType);
-    final String supervisor_State   [] = MainActivity.getInstance().getStringList(R.array.list_SupervisorState);
-    final String completion_Status  [] = MainActivity.getInstance().getStringList(R.array.list_CompletionStatus);
+    final String mains_Current_Type [] = MainActivity.getStringList(R.array.list_MainsCurrentType);
+    final String supervisor_State   [] = MainActivity.getStringList(R.array.list_SupervisorState);
+    final String completion_Status  [] = MainActivity.getStringList(R.array.list_CompletionStatus);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +67,8 @@ public class BcbActivity extends CanzeActivity implements FieldListener, DebugLi
 
     protected void initListeners() {
         MainActivity.getInstance().setDebugListener(this);
+        MainActivity.device.injectRequest(MainActivity.fields.getBySID(SID_TesterInit).getFrame());
+        addField(SID_TesterAwake, 1500);
         addField(SID_MainsCurrentType);
         addField(SID_Phase1currentRMS);
         addField(SID_Phase2CurrentRMS);
@@ -96,54 +102,54 @@ public class BcbActivity extends CanzeActivity implements FieldListener, DebugLi
                 switch (fieldId) {
 
                     case SID_MainsCurrentType:
-                        tv = (TextView) findViewById(R.id.textMainsCurrentType);
+                        tv = findViewById(R.id.textMainsCurrentType);
                         value = (int) field.getValue();
                         if (tv != null && mains_Current_Type != null && value >= 0 && value < mains_Current_Type.length)
                             tv.setText(mains_Current_Type[value]);
                         tv = null;
                         break;
                     case SID_Phase1currentRMS:
-                        tv = (TextView) findViewById(R.id.textPhase1CurrentRMS);
+                        tv = findViewById(R.id.textPhase1CurrentRMS);
                         break;
                     case SID_Phase2CurrentRMS:
-                        tv = (TextView) findViewById(R.id.textPhase2CurrentRMS);
+                        tv = findViewById(R.id.textPhase2CurrentRMS);
                         break;
                     case SID_Phase3CurrentRMS:
-                        tv = (TextView) findViewById(R.id.textPhase3CurrentRMS);
+                        tv = findViewById(R.id.textPhase3CurrentRMS);
                         break;
                     case SID_PhaseVoltage1:
-                        tv = (TextView) findViewById(R.id.textPhaseVoltage1);
+                        tv = findViewById(R.id.textPhaseVoltage1);
                         break;
                     case SID_PhaseVoltage2:
-                        tv = (TextView) findViewById(R.id.textPhaseVoltage2);
+                        tv = findViewById(R.id.textPhaseVoltage2);
                         break;
                     case SID_PhaseVoltage3:
-                        tv = (TextView) findViewById(R.id.textPhaseVoltage3);
+                        tv = findViewById(R.id.textPhaseVoltage3);
                         break;
                     case SID_InterPhaseVoltage12:
-                        tv = (TextView) findViewById(R.id.textInterPhaseVoltage12);
+                        tv = findViewById(R.id.textInterPhaseVoltage12);
                         break;
                     case SID_InterPhaseVoltage23:
-                        tv = (TextView) findViewById(R.id.textInterPhaseVoltage23);
+                        tv = findViewById(R.id.textInterPhaseVoltage23);
                         break;
                     case SID_InterPhaseVoltage31:
-                        tv = (TextView) findViewById(R.id.textInterPhaseVoltage31);
+                        tv = findViewById(R.id.textInterPhaseVoltage31);
                         break;
                     case SID_MainsActivePower:
-                        tv = (TextView) findViewById(R.id.textMainsActivePower);
+                        tv = findViewById(R.id.textMainsActivePower);
                         break;
                     case SID_GroundResistance:
-                        tv = (TextView) findViewById(R.id.textGroundResistance);
+                        tv = findViewById(R.id.textGroundResistance);
                         break;
                     case SID_SupervisorState:
-                        tv = (TextView) findViewById(R.id.textSupervisorState);
+                        tv = findViewById(R.id.textSupervisorState);
                         value = (int) field.getValue();
                         if (tv != null && supervisor_State != null && value >= 0 && value < supervisor_State.length)
                             tv.setText(supervisor_State[value]);
                         tv = null;
                         break;
                     case SID_CompletionStatus:
-                        tv = (TextView) findViewById(R.id.textCompletionStatus);
+                        tv = findViewById(R.id.textCompletionStatus);
                         value = (int) field.getValue();
                         if (tv != null && completion_Status != null && value >= 0 && value < completion_Status.length)
                             tv.setText(completion_Status[value]);
