211e2534fd88e714f28c0834099e5f83863f5693
Notice B.V <jmeijer@notice.nu>
2019-03-15 16:12:35 +0100

    Migrated to Sdk28 and AndroidX

--- app/src/main/java/lu/fisch/canze/fragments/ExperimentalFragment.java
+++ app/src/main/java/lu/fisch/canze/fragments/ExperimentalFragment.java
@@ -24,8 +24,8 @@ package lu.fisch.canze.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
