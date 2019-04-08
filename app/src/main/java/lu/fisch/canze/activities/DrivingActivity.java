d1f36becba31f826c0365210505800d821169324
Notice B.V <jmeijer@notice.nu>
2019-04-08 20:24:50 +0200

    Experiment to avoid in Android 8.1 in EditText, see manifest for details. Suppressed Manifest warnings for Google indexing

--- app/src/main/java/lu/fisch/canze/activities/DrivingActivity.java
+++ app/src/main/java/lu/fisch/canze/activities/DrivingActivity.java
@@ -26,11 +26,13 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -80,6 +82,13 @@ public class DrivingActivity extends CanzeActivity implements FieldListener, Deb
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT != 26 && Build.VERSION.SDK_INT != 27) {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driving);
 
