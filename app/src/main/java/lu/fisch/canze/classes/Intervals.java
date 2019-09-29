07cdb0e3edb11c9f500ccc86d441410066b4b9e2
Notice B.V <jmeijer@notice.nu>
2019-09-29 13:20:00 +0200

    Lint (Code->Analyse) on all canze:classes

--- app/src/main/java/lu/fisch/canze/classes/Intervals.java
+++ app/src/main/java/lu/fisch/canze/classes/Intervals.java
@@ -25,7 +25,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import lu.fisch.awt.Color;
