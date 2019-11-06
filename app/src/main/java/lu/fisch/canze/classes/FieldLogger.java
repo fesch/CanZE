ef674b2589245feb3e821c2542c2d6ec9ccf0776
Notice B.V <jmeijer@notice.nu>
2019-11-06 15:59:07 +0100

    Unified FieldLogger and DebugLogger code. Changelog

--- app/src/main/java/lu/fisch/canze/classes/FieldLogger.java
+++ app/src/main/java/lu/fisch/canze/classes/FieldLogger.java
@@ -13,6 +13,8 @@ import java.util.Locale;
 import lu.fisch.canze.R;
 import lu.fisch.canze.activities.MainActivity;
 
+import static lu.fisch.canze.activities.MainActivity.debug;
+
 /**
  * Created by Chris Mattheis on 03/11/15.
  * don't use yet - still work in progress
@@ -25,7 +27,8 @@ public class FieldLogger {
 
     private final static FieldLogger instance = new FieldLogger();
 
-    private FieldLogger() {}
+    private FieldLogger() {
+    }
 
     public static FieldLogger getInstance() {
         return instance;
@@ -35,30 +38,34 @@ public class FieldLogger {
      * Datalogger stuff
      * ****************************/
 
-    private File logFile = null;
-
+    private boolean isExternalStorageWritable() {
+        String SDstate = Environment.getExternalStorageState();
+        return (Environment.MEDIA_MOUNTED.equals(SDstate));
+    }
 
     private SimpleDateFormat sdf = new SimpleDateFormat(MainActivity.getStringSingle(R.string.format_YMDHMS), Locale.US);
+    private boolean firstLog = true;
+    private File logFile = null;
 
-    private boolean isCreated()
-    {
-        return (logFile!=null);
+    private boolean isCreated() {
+        return (logFile != null);
     }
 
     private boolean createNewLog() {
-        boolean result = false;
 
-        //debug(this.getClass().getSimpleName()+": create new field logfile");
-
-        // ensure that there is a CanZE Folder in SDcard
+        if (!MainActivity.storageIsAvailable) {
+            debug("AllDataActivity.createDump: SDcard not available");
+            return false;
+        }
+        if (!isExternalStorageWritable()) {
+            debug("DiagDump: SDcard not writeable");
+            return false;
+        }
         String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/CanZE/";
-
-        //debug(this.getClass().getSimpleName()+": file_path:" + file_path);
 
         File dir = new File(file_path);
         if (!dir.exists()) {
             dir.mkdirs();
-            //debug(this.getClass().getSimpleName()+": SDcard dir CanZE created");
         }
 
         String exportdataFileName = file_path + "field-" + sdf.format(Calendar.getInstance().getTime()) + ".log";
@@ -67,9 +74,10 @@ public class FieldLogger {
         if (!logFile.exists()) {
             try {
                 logFile.createNewFile();
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
+                logFile = null;
+                return false;
             }
         }
 
@@ -83,31 +91,33 @@ public class FieldLogger {
 
             sdf = new SimpleDateFormat(MainActivity.getStringSingle(R.string.format_YMDHMSs), Locale.US);
 
-            result = true;
-        }
-        catch (IOException e) {
+            return true;
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return result;
+        logFile = null;
+        return false;
     }
 
     /**
      * Appends a line of text to the log file
-     * @param text  the text line. A CR will be added automatically
+     *
+     * @param text the text line. A CR will be added automatically
      */
-    public void log(String text)
-    {
-        if(logFile==null) createNewLog();
+    public void log(String text) {
+        if (logFile == null && firstLog) createNewLog();
+        firstLog = false;
 
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(logFile, true));
-            bufferedWriter.append(sdf.format(Calendar.getInstance().getTime())+","+text+"\n");
-            bufferedWriter.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
+        if (logFile != null) {
+            try {
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(logFile, true));
+                bufferedWriter.append(sdf.format(Calendar.getInstance().getTime()) + "," + text + "\n");
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                logFile = null;
+            }
         }
     }
 
 }
-
