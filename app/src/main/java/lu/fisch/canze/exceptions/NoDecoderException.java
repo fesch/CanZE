59e440c562b2b999a538bba67d129b149f0f73c8
Notice B.V <jmeijer@notice.nu>
2019-11-24 12:33:58 +0100

    Code cleanup in package exceptions

--- app/src/main/java/lu/fisch/canze/exceptions/NoDecoderException.java
+++ app/src/main/java/lu/fisch/canze/exceptions/NoDecoderException.java
@@ -19,18 +19,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lu.fisch.canze.exceptions;
 
-/**
- *
- * @author robertfisch
- */
 public class NoDecoderException extends Exception {
-    
 }
