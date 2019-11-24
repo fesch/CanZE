6c84ba54dc7b4a48ea1fd4ec8567811f7f4dc97d
Notice B.V <jmeijer@notice.nu>
2019-11-24 12:31:46 +0100

    Code cleanup in package interfaces

--- app/src/main/java/lu/fisch/canze/interfaces/DrawSurfaceInterface.java
+++ app/src/main/java/lu/fisch/canze/interfaces/DrawSurfaceInterface.java
@@ -21,9 +21,6 @@
 
 package lu.fisch.canze.interfaces;
 
-/**
- * Created by robertfisch on 25.08.2015.
- */
 public interface DrawSurfaceInterface {
-    public void repaint();
+    void repaint();
 }
