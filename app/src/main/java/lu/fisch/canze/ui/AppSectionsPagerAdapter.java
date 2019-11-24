e75774d9d909806cafd36560c2653990096d4b47
Notice B.V <jmeijer@notice.nu>
2019-11-24 12:33:58 +0100

    Code cleanup in package exceptions

--- app/src/main/java/lu/fisch/canze/ui/AppSectionsPagerAdapter.java
+++ app/src/main/java/lu/fisch/canze/ui/AppSectionsPagerAdapter.java
@@ -25,16 +25,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-
 import java.util.ArrayList;
-
 import lu.fisch.canze.fragments.ExperimentalFragment;
 import lu.fisch.canze.fragments.MainFragment;
 import lu.fisch.canze.fragments.TechnicalFragment;
 
-/**
- * Created by robert fisch on 01.12.2015.
- */
 public class AppSectionsPagerAdapter extends FragmentPagerAdapter {
 
     private final ArrayList<Fragment> fragments = new ArrayList<>();
